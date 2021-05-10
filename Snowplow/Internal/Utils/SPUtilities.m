33059849e95cf74bd0192b92c95caa3000eebd14
Ronald DuCros <ronald.ducros@treatwell.com>
2021-05-10 13:36:52 +0100

    MAT-500: Disable snowplow isAdvertisingTrackingEnabled check

--- Snowplow/Internal/Utils/SPUtilities.m
+++ Snowplow/Internal/Utils/SPUtilities.m
@@ -109,8 +109,8 @@
     SEL isAdvertisingTrackingEnabledSelector = NSSelectorFromString(@"isAdvertisingTrackingEnabled");
     if (![sharedManager respondsToSelector:isAdvertisingTrackingEnabledSelector]) return nil;
 
-    BOOL isAdvertisingTrackingEnabled = ((BOOL (*)(id, SEL))[sharedManager methodForSelector:isAdvertisingTrackingEnabledSelector])(sharedManager, isAdvertisingTrackingEnabledSelector);
-    if (!isAdvertisingTrackingEnabled) return nil;
+//    BOOL isAdvertisingTrackingEnabled = ((BOOL (*)(id, SEL))[sharedManager methodForSelector:isAdvertisingTrackingEnabledSelector])(sharedManager, isAdvertisingTrackingEnabledSelector);
+//    if (!isAdvertisingTrackingEnabled) return nil;
 
     SEL advertisingIdentifierSelector = NSSelectorFromString(@"advertisingIdentifier");
     if (![sharedManager respondsToSelector:advertisingIdentifierSelector]) return nil;
